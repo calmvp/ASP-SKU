625d662c2bde0f7fdabdd963c590803ba0cbda0b
Ned <nkaidayip@gmail.com>
2013-10-07 14:34:49 -0400

    added simple views and controllers for manufacturer and unit

---
+++ MvcSKU/Controllers/LibraryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcSKU.Models;
+
+namespace MvcSKU.Controllers
+{
+    public class LibraryController : Controller
+    {
+        //
+        // GET: /Library/
+
+        public string Index()
+        {
+            return "Hello from Library Index";
+        }
+        //
+        // GET: /Library/Browse?manufacturer=Post
+        public ActionResult Browse(string manufacturer)
+        {
+            var manufacturerModel = new Manufacturer { Name = manufacturer };
+
+            return View(manufacturerModel);
+        }
+        //
+        // GET: /Library/Details/5
+        public ActionResult Details(int id)
+        {
+            var unit = new Unit { Name = "Unit " + id };
+
+            return View(unit);
+        }
+    }
+}
